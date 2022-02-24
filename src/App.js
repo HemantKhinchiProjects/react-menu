392227f8eaf49c820ef787723f68ea05c40b9449
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-02-24 20:54:16 +0530

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+import './style.css';
 
 export default function App() {
+  const [menuItem, setMenuItem] = useState(items);
+  const [categories, setCategories] = useState([]);
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItem} />
+      </section>
+    </main>
   );
 }
